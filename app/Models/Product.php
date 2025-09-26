52081ee407e8754306b4bb65c3653ca4082c8698
KZ68 <zitnik.kevin@gmail.com>
2025-09-26 16:17:57 +0200

    feat: add properties to Product

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -6,5 +6,35 @@ use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
-    
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var list<string>
+     */
+    protected $fillable = [
+        'name',
+        'description',
+        'reference',
+        'ean13',
+        'quantity',
+        'retailPrice',
+        'active'
+    ];
+
+    /**
+     * Get the attributes that should be cast.
+     *
+     * @return array<string, string>
+     */
+    protected function casts(): array
+    {
+        return [
+            'name' => 'string',
+            'description' => 'string',
+            'reference' => 'string',
+            'ean13' => 'string',
+            'retailPrice' => 'float',
+            'active' => 'boolean',
+        ];
+    }
 }

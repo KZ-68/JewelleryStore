a85475b011058b276f6222543843e4e594a7781c
KZ68 <zitnik.kevin@gmail.com>
2025-09-26 16:19:02 +0200

    feat: add migration file for Product Model

---
+++ database/migrations/2025_09_26_135227_create_products_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    use SoftDeletes;
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 255);
+            $table->string('description', 255);
+            $table->string('reference', 100);
+            $table->string('ean13', 13);
+            $table->integer('quantity');
+            $table->float('retailPrice', 2);
+            $table->boolean('active');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('products');
+    }
+};

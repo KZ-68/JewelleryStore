c5038ecb9fd9e97dfa4c3140387fbeadc64494be
Kevin ZITNIK <zitnik.kevin@gmail.com>
2025-10-02 23:55:57 +0200

    feat: add adress migration table

---
+++ database/migrations/2025_10_02_211029_create_addresses_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('addresses', function (Blueprint $table) {
+            $table->id();
+            $table->string('details_1', 255);
+            $table->string('details_2', 255);
+            $table->string('postal_code', 10);
+            $table->date('deleted_at');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('addresses');
+    }
+};

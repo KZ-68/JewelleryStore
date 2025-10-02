bcc7079dcd914d5a1ecfb03896d7d27c080f6c2f
Kevin ZITNIK <zitnik.kevin@gmail.com>
2025-10-02 23:59:38 +0200

    feat: add contact and cart routes

--- routes/web.php
+++ routes/web.php
@@ -11,5 +11,13 @@ Route::get('dashboard', function () {
     return Inertia::render('Dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
+Route::get('/cart', function () {
+    return Inertia::render('Cart');
+})->name('cart');
+
+Route::get('/contact', function () {
+    return Inertia::render('Contact');
+})->name('contact');
+
 require __DIR__.'/settings.php';
 require __DIR__.'/auth.php';

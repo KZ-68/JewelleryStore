d997fcbf0f02033c3579a43f67b384e3d083c5b8
KZ68 <zitnik.kevin@gmail.com>
2025-10-03 16:45:39 +0200

    feat: add Admin Controllers

---
+++ app/Http/Controllers/Auth/admin/AdminAuthenticatedSessionController.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Http\Controllers\Auth\Admin;
+
+use App\Models\User;
+use Inertia\Inertia;
+use Inertia\Response;
+use Illuminate\Http\Request;
+use Laravel\Fortify\Features;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Support\Facades\Route;
+use App\Http\Requests\Auth\LoginRequest;
+
+class AdminAuthenticatedSessionController extends Controller
+{
+    /**
+     * Show the login page.
+     */
+    public function create(Request $request): Response
+    {
+        return Inertia::render('auth/admin/AdminLogin', [
+            'canResetPassword' => Route::has('password.request'),
+            'status' => $request->session()->get('status'),
+        ]);
+    }
+
+    /**
+     * Handle an incoming authentication request.
+     */
+    public function store(LoginRequest $request): RedirectResponse
+    {
+        $user = $request->validateCredentials();
+
+        if (Features::enabled(Features::twoFactorAuthentication()) && $user->hasEnabledTwoFactorAuthentication()) {
+            $request->session()->put([
+                'login.id' => $user->getKey(),
+                'login.remember' => $request->boolean('remember'),
+            ]);
+
+            return to_route('two-factor.admin-login');
+        }
+
+        Auth::login($user, $request->boolean('remember'));
+
+        $request->session()->regenerate();
+
+        if ($user instanceof User) {
+            return redirect()->route('showDashboard');
+        }
+
+        return redirect()->route('home');
+    }
+
+    /**
+     * Destroy an authenticated session.
+     */
+    public function destroy(Request $request): RedirectResponse
+    {
+        Auth::guard('admin')->logout();
+
+        $request->session()->invalidate();
+        $request->session()->regenerateToken();
+
+        return redirect('/');
+    }
+}
---
+++ app/Http/Controllers/Auth/admin/AdminEmailVerificationPromptController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers\Auth\Admin;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
+use Inertia\Inertia;
+use Inertia\Response;
+
+class AdminEmailVerificationPromptController extends Controller
+{
+    /**
+     * Show the email verification prompt page.
+     */
+    public function __invoke(Request $request): RedirectResponse|Response
+    {
+        return $request->user()->hasVerifiedEmail()
+                    ? redirect()->intended(route('home', absolute: false))
+                    : Inertia::render('auth/admin/AdminVerifyEmail', ['status' => $request->session()->get('status')]);
+    }
+}

f8b01c81d5c488bef10db63d908d7084604b5616
Kevin ZITNIK <zitnik.kevin@gmail.com>
2025-10-09 23:56:41 +0200

    feat: add logout button component

---
+++ resources/js/components/jewelery_store/button/LogoutButton.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts">
+import type { HTMLAttributes } from 'vue'
+import { cn } from '@/lib/utils'
+import { Primitive, type PrimitiveProps } from 'reka-ui'
+import { type ButtonVariants, buttonVariants } from '.'
+import axios from 'axios';
+
+interface Props extends PrimitiveProps {
+  variant?: ButtonVariants['variant']
+  size?: ButtonVariants['size']
+  class?: HTMLAttributes['class']
+}
+
+const props = withDefaults(defineProps<Props>(), {
+  as: 'button',
+})
+
+function submit() {
+    axios.post('/logout')
+        .then((res) => {
+        console.log(res.data)
+    })
+}
+</script>
+
+<template>
+  <Primitive
+    data-slot="button"
+    :as="as"
+    :as-child="asChild"
+    :class="cn(buttonVariants({ variant, size }), props.class)"
+    @click="submit"
+  >
+    <slot />
+  </Primitive>
+</template>

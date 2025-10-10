145a2b924bc4be41b9f469010f5d44ee605d3084
Kevin ZITNIK <zitnik.kevin@gmail.com>
2025-10-10 23:25:10 +0200

    feat: add Cart page vue file

---
+++ resources/js/pages/web/Cart.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import CartLeftColumn from '../../components/jewellery_store/cart/left_column/CartLeftColumn.vue';
+
+</script>
+
+<template>
+    <div
+        class="items-center min-h-screen bg-[#FDFDFC] p-6 text-[#1b1b18] lg:justify-center lg:p-8 bg-neutral-200 dark:bg-[#0a0a0a]"
+    >
+    <CartLeftColumn/>
+    </div>
+</template>

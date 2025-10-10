dadcb6bfa65463348328d0b4c8ac40f011bd56ab
Kevin ZITNIK <zitnik.kevin@gmail.com>
2025-10-10 23:27:55 +0200

    feat: update cart route to controller

--- routes/web.php
+++ routes/web.php
@@ -1,15 +1,15 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Web\CartController;
+use App\Http\Controllers\Admin\DashboardController;
 
 Route::get('/', function () {
     return Inertia::render('Welcome');
 })->name('home');
 
-Route::get('/cart', function () {
-    return Inertia::render('Cart');
-})->name('cart');
+Route::get('web/cart', [CartController::class, 'show'])->name('cart.show');
 
 Route::get('/contact', function () {
     return Inertia::render('Contact');

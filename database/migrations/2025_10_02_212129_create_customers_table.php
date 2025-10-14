18cd026047ce5f260a6c3c4ddba682a59e6333f8
Kevin ZITNIK <zitnik.kevin@gmail.com>
2025-10-14 23:55:05 +0200

    feat: update properties for customer table

--- database/migrations/2025_10_02_212129_create_customers_table.php
+++ database/migrations/2025_10_02_212129_create_customers_table.php
@@ -13,8 +13,7 @@ return new class extends Migration
     {
         Schema::create('customers', function (Blueprint $table) {
             $table->id();
-            $table->string('firstname')->nullable();
-            $table->string('lastname')->nullable();
+            $table->string('name')->nullable();
             $table->string('email')->unique()->nullable();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');

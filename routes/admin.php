219900f7ee9c47b778d8856d8480776f18124916
Kevin ZITNIK <zitnik.kevin@gmail.com>
2025-10-14 23:52:36 +0200

    refactor: remove unrelevant admin route file

--- routes/admin.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-use App\Http\Controllers\Admin\BackOfficeController;
-use Illuminate\Support\Facades\Route;
-
-Route::middleware(['admin.session', 'auth:admin'])->group(function () {
-    Route::get('admin/back-office', [BackOfficeController::class, 'show'])->name('bo.show');
-});

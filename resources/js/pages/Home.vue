2dd88407c5022c554e0a9072a0bf3fca60911216
Kevin ZITNIK <zitnik.kevin@gmail.com>
2025-10-14 23:50:37 +0200

    refactor: update

---
+++ resources/js/pages/Home.vue
@@ -0,0 +1,59 @@
+<script setup lang="ts">
+import { login, register } from '@/routes';
+import { show }  from '@/routes/bo/index'
+import { Head, Link } from '@inertiajs/vue3';
+import LogoutButton from '@/components/jewellery_store/button/LogoutButton.vue'
+</script>
+
+<template>
+    <Head title="Home">
+        <link rel="preconnect" href="https://rsms.me/" />
+        <link rel="stylesheet" href="https://rsms.me/inter/inter.css" />
+    </Head>
+    <div
+        class="flex min-h-screen flex-col items-center bg-[#FDFDFC] text-[#1b1b18] lg:justify-center dark:bg-[#0a0a0a]"
+    >
+        <header
+            class="mb-6 w-full text-sm not-has-[nav]:hidden"
+        >
+            <div id="banner" class="pt-3 pb-3">
+
+            </div>
+            <nav class="flex items-center justify-end gap-4 px-4">
+                <Link
+                    
+                    :href="show()"
+                    class="inline-block rounded-sm border border-[#19140035] px-5 py-1.5 text-sm leading-normal text-[#1b1b18] hover:border-[#1915014a] dark:border-[#3E3E3A] dark:text-[#EDEDEC] dark:hover:border-[#62605b]"
+                >
+                    Dashboard
+                </Link>
+                <p>{{ $page.props.auth.user }}</p>
+                <LogoutButton v-if="$page.props.auth.user">Logout</LogoutButton>
+                <template v-else>
+                    <Link
+                        :href="login()"
+                        class="inline-block rounded-sm border border-transparent px-5 py-1.5 text-sm leading-normal text-[#1b1b18] hover:border-[#19140035] dark:text-[#EDEDEC] dark:hover:border-[#3E3E3A]"
+                    >
+                        Log in
+                    </Link>
+                    <Link
+                        :href="register()"
+                        class="inline-block rounded-sm border border-[#19140035] px-5 py-1.5 text-sm leading-normal text-[#1b1b18] hover:border-[#1915014a] dark:border-[#3E3E3A] dark:text-[#EDEDEC] dark:hover:border-[#62605b]"
+                    >
+                        Register
+                    </Link>
+                </template>
+            </nav>
+        </header>
+        <div
+            class="flex w-full items-center justify-center opacity-100 transition-opacity duration-750 lg:grow starting:opacity-0"
+        >
+            <main
+                class="flex w-full max-w-[335px] flex-col-reverse overflow-hidden rounded-lg lg:max-w-4xl lg:flex-row"
+            >
+                
+            </main>
+        </div>
+        <div class="hidden h-14.5 lg:block"></div>
+    </div>
+</template>

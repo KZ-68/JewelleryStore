6d4c0e1467041d7aaffa491de540cc33f535c09c
Kevin ZITNIK <zitnik.kevin@gmail.com>
2025-10-16 00:32:23 +0200

    feat: add routes for privacy et contact post

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use Inertia\Inertia;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Web\CartController;
+use App\Http\Controllers\Web\ContactController;
 use App\Http\Controllers\Admin\BackOfficeController;
 
 Route::get('/', function () {
@@ -11,14 +12,16 @@ Route::get('/', function () {
 
 Route::get('/cart', [CartController::class, 'show'])->name('cart.show');
 
-Route::get('/contact', function () {
-    return Inertia::render('Contact');
-})->name('contact');
+Route::get('contact', [ContactController::class, 'create'])->name('contact.create');
 
-Route::prefix('admin')->group(function () {
-    Route::middleware('auth:admin')->group(function () {
-        Route::get('back-office', [BackOfficeController::class, 'showBO'])->name('showBO');
-    });
+Route::post('contact', [ContactController::class, 'store'])->name('contact.store');
+
+Route::get('privacy', function () {
+    return Inertia::render('web/Privacy');
+})->name('privacy');
+
+Route::group(['middleware' => ['role:admin', 'auth:admin']], function () {
+    Route::get('/admin/back-office', [BackOfficeController::class, 'showBO'])->name('admin.back-office.showBO');
 });
 
 require __DIR__.'/settings.php';

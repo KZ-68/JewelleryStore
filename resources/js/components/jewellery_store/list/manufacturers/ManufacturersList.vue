2fd10ca83f278165f482fc1eae4d93706b75638e
Kevin ZITNIK <zitnik.kevin@gmail.com>
2025-10-23 23:59:42 +0200

    feat: add ManufacturersList filter data

--- resources/js/components/jewellery_store/list/manufacturers/ManufacturersList.vue
+++ resources/js/components/jewellery_store/list/manufacturers/ManufacturersList.vue
@@ -1,19 +1,29 @@
 <script setup lang="ts">
 import type { Manufacturer } from '@/types/manufacturer'
 
-interface ManufacturersListProps {
-    classname:string;
-    manufacturers: Manufacturer[]
-}    
 
-defineProps<ManufacturersListProps>();
+const props = defineProps<{
+    manufacturers: Manufacturer[]
+    sortBy: string
+    order: 'asc' | 'desc'
+}>()
+
+// const emit = defineEmits<{
+//   (e: 'navigate', url: string): void
+// }>() 
+
 </script>
 
 <template>
     <section id="manufacturers-list-wrapper" class="bg-gray-100 rounded-lg py-4 px-12">
         <h2 class="text-3xl my-6">Manufacturers</h2>
-        <ul id="manufacturers-list" class="flex flex-col gap-4">
-            <li v-for="manufacturer in manufacturers" class="bg-white rounded-md py-8 px-5 my-3">{{ manufacturer.name }}</li>
+        <ul v-if="props.manufacturers.length > 0" id="manufacturers-list" class="flex flex-col gap-4">
+            <li v-for="manufacturer in props.manufacturers" v-bind:key="manufacturer.id" class="bg-white rounded-md py-8 px-5 my-3">{{ manufacturer.name }}</li>
+        </ul>
+        <ul v-else id="manufacturers-list" class="flex flex-col gap-4">
+            <li class="bg-white rounded-md py-8 px-5 my-3">
+                <p>No Manufacturer registered</p>
+            </li>
         </ul>
     </section>
 </template>

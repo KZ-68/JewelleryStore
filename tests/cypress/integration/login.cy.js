d40d03e40283f8e1841ed9d72138eaa2e4d0dfc2
KZ68 <zitnik.kevin@gmail.com>
2025-10-24 09:54:43 +0200

    feat: add boilerplate cypress files

---
+++ tests/cypress/integration/login.cy.js
@@ -0,0 +1,31 @@
+describe('Connexion', () => {
+  const baseUrl = 'http://localhost:8000'; // Laravel + Inertia est souvent servi sur 8000
+
+  beforeEach(() => {
+    cy.visit(`${baseUrl}/admin/login`);
+  });
+
+  it('Affiche les champs de connexion', () => {
+    cy.contains('Log in to your account').should('exist');
+    cy.get('input[name="email"]').should('exist');
+    cy.get('input[name="password"]').should('exist');
+    cy.get('button[type="submit"]').should('exist');
+  });
+
+  it('Refuse une connexion avec de mauvais identifiants', () => {
+    cy.get('input[name="email"]').type('fake@exemple.com');
+    cy.get('input[name="password"]').type('mauvaismotdepasse');
+    cy.get('button[data-slot="button"][type="submit"]').click();
+
+    cy.get('body').should('contain', 'Identifiants invalides');
+  });
+
+  it('Connexion réussie et redirection vers /admin/back-office', () => {
+    cy.get('input[name="email"]').type('jean@example.com');
+    cy.get('input[name="password"]').type('password');
+    cy.get('button[data-slot="button"][type="submit"]').click();
+
+    cy.url({ timeout: 10000 }).should('include', '/admin/back-office');
+    cy.contains('Laravel Starter Kit').should('exist');
+  });
+});

6ef9ca75d9b86101c594ff9b3b774fb6509e572b
Kevin ZITNIK <zitnik.kevin@gmail.com>
2025-10-31 10:26:52 +0100

    refactor: add project root path dynamism

--- cypress.config.js
+++ cypress.config.js
@@ -1,20 +1,23 @@
+const path = require('path')
 const { defineConfig } = require('cypress')
 
+const projectRoot = __dirname // racine du dépôt
+
 module.exports = defineConfig({
-    chromeWebSecurity: false,
-    retries: 2,
-    defaultCommandTimeout: 5000,
-    watchForFileChanges: false,
-    downloadsFolder: 'tests/cypress/downloads',
-    videosFolder: 'tests/cypress/videos',
-    screenshotsFolder: 'tests/cypress/screenshots',
-    fixturesFolder: 'tests/cypress/fixture',
-    e2e: {
-        setupNodeEvents(on, config) {
-            return require('./tests/cypress/plugins/index.js')(on, config)
-        },
-        baseUrl: 'http://localhost:8000',
-        specPattern: 'tests/cypress/integration/**/*.cy.{js,jsx,ts,tsx}',
-        supportFile: 'tests/cypress/support/e2e.js',
+  chromeWebSecurity: false,
+  retries: 2,
+  defaultCommandTimeout: 5000,
+  watchForFileChanges: false,
+  downloadsFolder: path.join(projectRoot, 'tests/cypress/downloads'),
+  videosFolder: path.join(projectRoot, 'tests/cypress/videos'),
+  screenshotsFolder: path.join(projectRoot, 'tests/cypress/screenshots'),
+  fixturesFolder: path.join(projectRoot, 'tests/cypress/fixtures'),
+  e2e: {
+    setupNodeEvents(on, config) {
+      return require(path.join(projectRoot, 'tests/cypress/plugins/index.js'))(on, config)
     },
+    baseUrl: 'http://localhost:8000',
+    specPattern: path.join(projectRoot, 'tests/cypress/integration/**/*.cy.{js,jsx,ts,tsx}'),
+    supportFile: path.join(projectRoot, 'tests/cypress/support/e2e.js'),
+  },
 })

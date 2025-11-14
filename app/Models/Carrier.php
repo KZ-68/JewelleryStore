4689cb66427bef555dee8987d09cfee3ac651df0
Kevin ZITNIK <zitnik.kevin@gmail.com>
2025-11-14 13:54:40 +0100

    feat: add Carrier Model file

---
+++ app/Models/Carrier.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Carrier extends Model
+{
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var list<string>
+     */
+    protected $fillable = [
+        'name',
+        'description',
+        'carrier_position',
+    ];
+
+    /**
+     * Get the attributes that should be cast.
+     *
+     * @return array<string, string>
+     */
+    protected function casts(): array
+    {
+        return [
+            'created_at' => 'datetime:Y-m-d H:i:s',
+            'updated_at' => 'datetime:Y-m-d H:i:s'
+        ];
+    }
+}

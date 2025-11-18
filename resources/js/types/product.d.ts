a0d82dbc3d9cadf8e41a677c83f31390b7b0b7cc
Kevin ZITNIK <zitnik.kevin@gmail.com>
2025-11-18 01:31:22 +0100

    feat: update product type file

--- resources/js/types/product.d.ts
+++ resources/js/types/product.d.ts
@@ -1,12 +1,13 @@
 export interface Product {
     id: number
     name: string
-    description: string,
-    reference: string,
-    ean13: string,
-    quantity: number,
-    retailPrice: number,
-    active: boolean,
-    created_at: string;
-    updated_at: string;
+    slug: string
+    description: string
+    reference: string
+    ean13: string
+    quantity: number
+    retailPrice: number
+    active: boolean
+    created_at: string
+    updated_at: string
 }

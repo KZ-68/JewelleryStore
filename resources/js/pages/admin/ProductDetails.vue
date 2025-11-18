3f01d60cb47b310dcf4ed4e2a4486bea045b226e
Kevin ZITNIK <zitnik.kevin@gmail.com>
2025-11-18 01:41:47 +0100

    feat: add ProductDetails component file

---
+++ resources/js/pages/admin/ProductDetails.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import AdminProductMainForm from '@/components/jewellery_store/form/AdminProductMainForm.vue';
+import { Head } from '@inertiajs/vue3';
+import type { Product } from '@/types/product'
+
+interface ProductDetailsProps {
+  product: Product
+}  
+
+const props = defineProps<ProductDetailsProps>()
+</script>
+
+
+<template>
+    <Head :title=props.product.name />
+    
+    <AdminProductMainForm classname="" :product=props.product></AdminProductMainForm>
+</template>

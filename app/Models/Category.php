d25d8d33faf3f9355a0aa4f05e8ea7a6a03dcff6
Kevin ZITNIK <zitnik.kevin@gmail.com>
2025-11-20 23:44:43 +0100

    feat: add Category Model

---
+++ app/Models/Category.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Symfony\Component\String\Slugger\SluggerInterface;
+
+class Category extends Model
+{
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var list<string>
+     */
+    protected $fillable = [
+        'parent_id',
+        'name',
+        'description',
+        'category_position',
+        'index'
+    ];
+
+    /**
+     * Get the attributes that should be cast.
+     *
+     * @return array<string, string>
+     */
+    protected function casts(): array
+    {
+        return [
+            'created_at' => 'datetime:Y-m-d H:i:s',
+            'updated_at' => 'datetime:Y-m-d H:i:s'
+        ];
+    }
+}

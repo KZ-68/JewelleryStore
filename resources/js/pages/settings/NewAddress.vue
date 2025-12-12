7b8b440f348b13d8036ba8f8ff8e2a9c9e14a409
KZ68 <zitnik.kevin@gmail.com>
2025-12-12 16:35:56 +0100

    refactor: change name header

--- resources/js/pages/settings/NewAddress.vue
+++ resources/js/pages/settings/NewAddress.vue
@@ -18,7 +18,7 @@ const props = defineProps<NewAddressProps>()
   <AppLayout>
     <section id="new-carrier-page-wrapper" class="flex flex-col mx-2">
       <hgroup id="new-carrier-page-top" class="mx-4 mt-8 mb-4">
-        <h1 id="new-carrier-page-heading" class="text-xl">Add a new carrier</h1>            
+        <h1 id="new-carrier-page-heading" class="text-xl">Add a new address</h1>            
       </hgroup>
       <AdminAddressCreateForm classname="" :countries="countries"></AdminAddressCreateForm>
       <Link :href="showAddresses()" class="w-fit inline-block rounded-sm border border-[#19140035] px-5 py-1.5 mx-6 my-8 text-sm leading-normal text-[#1b1b18] hover:border-[#1915014a] dark:border-[#3E3E3A] dark:text-[#EDEDEC] dark:hover:border-[#62605b]">

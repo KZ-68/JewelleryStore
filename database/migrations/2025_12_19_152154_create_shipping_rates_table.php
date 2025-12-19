011a3d8c21e6f2f729a1dfddf66fa5c43814db13
KZ68 <zitnik.kevin@gmail.com>
2025-12-19 16:48:09 +0100

    feat: add shipping rate migration

---
+++ database/migrations/2025_12_19_152154_create_shipping_rates_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('shipping_rates', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('id_carrier')->nullable()->constrained('carriers');
+            $table->foreignId('id_country')->nullable()->constrained('countries');
+            $table->float('min_total', 2);
+            $table->float('max_total', 2);
+            $table->float('price', 2);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('shipping_rates');
+    }
+};

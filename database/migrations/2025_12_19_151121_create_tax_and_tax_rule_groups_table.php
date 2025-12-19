50ef8d4bf823f3b8ec05bc1d1a94f78618721746
KZ68 <zitnik.kevin@gmail.com>
2025-12-19 16:47:37 +0100

    feat: add tax, tax_rule and tax_rule_group migration

---
+++ database/migrations/2025_12_19_151121_create_tax_and_tax_rule_groups_table.php
@@ -0,0 +1,47 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('taxes', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 128);
+            $table->float('percentage', 2);
+            $table->boolean('applicable')->default(true);
+            $table->string('type', 20);
+            $table->string('description', 128);
+            $table->integer('rate_order');
+        });
+
+        Schema::create('tax_rules', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('id_tax')->nullable()->constrained('taxes');
+            $table->foreignId('id_country')->nullable()->constrained('countries');
+            $table->string('bahavior', 20);
+            $table->integer('rate_order');
+        });
+
+        Schema::create('tax_rule_groups', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('id_tax_rule')->nullable()->constrained('tax_rules');
+            $table->string('name', 255);
+            $table->boolean('active')->default(true);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('tax_rule_groups');
+    }
+};

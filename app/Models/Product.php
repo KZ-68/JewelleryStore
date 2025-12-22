6dfe8e3622b7e643aa8319a156fd56d6338e3c1a
Kevin ZITNIK <zitnik.kevin@gmail.com>
2025-12-22 22:31:59 +0100

    feat: add TaxRuleGroup relationship

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -2,8 +2,9 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Str;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Product extends Model
@@ -51,6 +52,16 @@ class Product extends Model
     public function categories(): BelongsToMany
     {
         return $this->belongsToMany(Category::class, 'category_product', 'product_id', 'category_id')->withTimestamps();
+    }
+
+    public function taxRuleGroup(): BelongsTo
+    {
+        return $this->belongsTo(TaxRuleGroup::class);
+    }
+
+    public function seller(): BelongsTo
+    {
+        return $this->belongsTo(Seller::class);
     }
 
     /**

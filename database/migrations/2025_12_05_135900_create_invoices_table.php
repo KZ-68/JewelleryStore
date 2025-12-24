30be6d068736cb74b67308efe0bb57a3c53022e6
Kevin ZITNIK <zitnik.kevin@gmail.com>
2025-12-24 16:03:28 +0100

    feat: add payement foreign key

--- database/migrations/2025_12_05_135900_create_invoices_table.php
+++ database/migrations/2025_12_05_135900_create_invoices_table.php
@@ -13,6 +13,7 @@ return new class extends Migration
     {
         Schema::create('invoices', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('id_payment')->nullable()->constrained('payments');
             $table->integer('number');
             $table->timestamps();
         });

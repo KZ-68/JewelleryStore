75908831e4e13a135fe0c49e8d1e64cecbe1b06a
Kevin ZITNIK <zitnik.kevin@gmail.com>
2026-01-04 21:50:57 +0100

    feat: add groups props

--- resources/js/pages/admin/CustomerAdminSettings.vue
+++ resources/js/pages/admin/CustomerAdminSettings.vue
@@ -2,11 +2,13 @@
 import AppLayout from '@/layouts/AppLayout.vue';
 import AdminCustomerMainForm from '@/components/jewellery_store/form/AdminCustomerMainForm.vue';
 import { Head, Link } from '@inertiajs/vue3';
-import type { Customer } from '@/types/customer'
+import type { Customer } from '@/types/customer';
+import type { Group } from '@/types/group';
 import { showCustomers } from '@/routes/admin/back-office';
 
 interface CustomerAdminSettings {
   customer: Customer
+  groups: Group[]
 }  
 
 const props = defineProps<CustomerAdminSettings>()
@@ -16,7 +18,7 @@ const props = defineProps<CustomerAdminSettings>()
 <template>
     <Head :title=props.customer.name />
     <AppLayout>
-      <AdminCustomerMainForm classname="" :customer=props.customer></AdminCustomerMainForm>
+      <AdminCustomerMainForm classname="" :customer=props.customer :groups=props.groups></AdminCustomerMainForm>
       <Link :href="showCustomers()" class="w-fit inline-block rounded-sm border border-[#19140035] px-5 py-1.5 mx-6 my-8 text-sm leading-normal text-[#1b1b18] hover:border-[#1915014a] dark:border-[#3E3E3A] dark:text-[#EDEDEC] dark:hover:border-[#62605b]">
           Back to customers list
       </Link>

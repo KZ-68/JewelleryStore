9d93389fc201c25c73368a6dbd6986cc2ddaeae1
KZ68 <zitnik.kevin@gmail.com>
2026-01-12 16:41:34 +0100

    feat: add orders relationship

--- app/Models/Invoice.php
+++ app/Models/Invoice.php
@@ -2,20 +2,30 @@
 
 namespace App\Models;
 
+use App\Models\Order;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Invoice extends Model
 {
-    use SoftDeletes;
     /**
      * The attributes that are mass assignable.
      *
      * @var list<string>
      */
     protected $fillable = [
+        'id',
         'number',
     ];
+
+        
+    /**
+    * The orders that belong to the invoice.
+    */
+    public function orders(): BelongsToMany
+    {
+        return $this->belongsToMany(Order::class, 'invoice_order', 'invoice_id', 'order_id')->withTimestamps();
+    }
 
     /**
      * Get the attributes that should be cast.

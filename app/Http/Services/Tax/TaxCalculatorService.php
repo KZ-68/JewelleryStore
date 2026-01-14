30a6f07880910e152bb4c2f8864899105c16a733
KZ68 <zitnik.kevin@gmail.com>
2026-01-14 16:48:46 +0100

    feat: add tax calculation class

---
+++ app/Http/Services/Tax/TaxCalculatorService.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Services\Tax;
+
+class TaxCalculatorService
+{
+    public function calculateTax(float $amount, float $rate = 0.20): float
+    {
+        return round($amount * $rate, 2);
+    }
+
+    public function withTax(float $amount, float $rate = 0.20): float
+    {
+        return round($amount + $this->calculateTax($amount, $rate), 2);
+    }
+}

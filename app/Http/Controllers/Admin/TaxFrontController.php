c16bbe96b25b3ce394eeb2246c79be8f9fd85944
KZ68 <zitnik.kevin@gmail.com>
2026-01-14 16:42:53 +0100

    feat: add Tax Front Controller file

---
+++ app/Http/Controllers/Admin/TaxFrontController.php
@@ -0,0 +1,116 @@
+<?php
+/* 
+* Front Controller File for the tax page connected to the Back Office section 
+*/
+
+namespace App\Http\Controllers\Admin;
+
+use Inertia\Inertia;
+use Inertia\Response;
+use App\Models\Tax;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+use App\Http\Controllers\Controller;
+
+class TaxFrontController extends Controller
+{
+    /**
+    * Show tax details view
+    * @param Request Get the request, via GET method
+    * @return Response|RedirectResponse Return an Inertia Object response with the rendered view or a redirection
+    */
+    public function show(Request $request): Response|RedirectResponse
+    {
+        $tax = Tax::where('name', $request->name)->firstOrFail();
+
+        if(!$tax) {
+            redirect('not-found', 404);
+        }
+
+        return Inertia::render('admin/TaxDetails', [
+            'tax' => $tax
+        ]);
+    }
+
+    /**
+    * Tax creation form view
+    * @param Request Get the request, via GET method
+    * @return Response|RedirectResponse Return an Inertia Object response with the rendered view or a redirection
+    */
+    public function newTax(Request $request): Response|RedirectResponse
+    {
+        return Inertia::render('admin/NewTax', []);
+    }
+
+    /**
+    * This method validate every fields used in the new tax form.
+    * @param Request Get the POST method body from the form
+    * @return RedirectResponse Send a response with a redirection
+    */
+    public function create(Request $request): RedirectResponse
+    {
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|string|max:255',
+            'rate' => 'required|float',     
+            'applicable' => 'required|boolean',
+            'type' => 'required|string|max:20',
+            'description' => 'string|max:128'
+        ]);
+
+        if ($validator->fails()) {
+            return redirect('/admin/back-office/taxes')
+                ->withErrors($validator)
+                ->withInput();
+        }
+
+        $tax = new Tax;
+        $tax = Tax::where('name', $request->get('name'))->first();
+        $taxRatePercent = $request->get('rate') * 100;
+        $tax->name = `{$request->get('type')} $taxRatePercent`;
+        $tax->rate = $request->get('rate');
+        $tax->applicable = $request->get('applicable');
+        $tax->type = $request->get('type');
+        if($request->get('description') !== null) {
+            $tax->description = $request->get('description');
+        }
+
+        $tax->save();
+
+        return redirect('/admin/back-office/taxes');
+    }
+
+    /**
+    * This method validate every fields used in the product details form.
+    * @param Request Get the POST method body from the form
+    * @return RedirectResponse Send a response with a redirection
+    */
+    public function update(Request $request): RedirectResponse
+    {
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|string|max:255',
+            'rate' => 'required|float',     
+            'applicable' => 'required|boolean',
+            'type' => 'required|string|max:20',
+            'description' => 'string|max:128'
+        ]);
+
+        if ($validator->fails()) {
+            return redirect('/admin/back-office/taxes')
+                ->withErrors($validator)
+                ->withInput();
+        }
+
+        $tax = Tax::where('name', $request->get('name'))->first();
+        $tax->rate = $request->get('rate');
+        $tax->applicable = $request->get('applicable');
+        $tax->type = $request->get('type');
+        if($request->get('description') !== null) {
+            $tax->description = $request->get('description');
+        }
+
+        $tax->save();
+
+        return redirect('/admin/back-office/taxes');
+    }
+}

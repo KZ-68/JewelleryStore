09eeb97fd3eef8abbb1c5d6f9bb87fdd15d08296
KZ68 <zitnik.kevin@gmail.com>
2026-01-14 16:43:40 +0100

    feat: add slug and deleted_at column

--- app/Models/Tax.php
+++ app/Models/Tax.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use App\Models\TaxRule;
+use Illuminate\Support\Str;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Database\Eloquent\Relations\HasMany;
@@ -17,11 +18,28 @@ class Tax extends Model
      */
     protected $fillable = [
         'name',
-        'percentage',
+        'rate',
         'applicable',
         'type',
         'description'
     ];
+
+        // Create a slug automaticaly after the creation of the tax
+    public static function boot()
+    {
+        parent::boot();
+
+        static::creating(function ($tax) {
+            $slug = Str::slug($tax->name);
+
+            $count = Tax::where('slug', $slug)->count();
+            if ($count) {
+                $slug .= '-' . ($count + 1);
+            }
+
+            $tax->slug = $slug;
+        });
+    }
 
     /**
      * Get the attributes that should be cast.
@@ -32,7 +50,8 @@ class Tax extends Model
     {
         return [
             'created_at' => 'datetime:Y-m-d H:i:s',
-            'updated_at' => 'datetime:Y-m-d H:i:s'
+            'updated_at' => 'datetime:Y-m-d H:i:s',
+            'deleted_at' => 'datetime:Y-m-d H:i:s'
         ];
     }
 

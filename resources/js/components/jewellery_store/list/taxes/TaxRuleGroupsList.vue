3506a3dc610babad25912601afd516c90dca03ea
KZ68 <zitnik.kevin@gmail.com>
2026-01-14 16:46:37 +0100

    feat: add template and component for tax and tax rule group

---
+++ resources/js/components/jewellery_store/list/taxes/TaxRuleGroupsList.vue
@@ -0,0 +1,47 @@
+<script setup lang="ts">
+import type { TaxRuleGroup } from '@/types/taxRuleGroup';
+import { Link } from '@inertiajs/vue3'
+import { route } from '../../../../../../vendor/tightenco/ziggy/src/js';
+import { Ziggy } from '../../../../ziggy.js';
+import { FileEditIcon } from 'lucide-vue-next';
+
+interface TaxRuleGroupsListProps {
+    classname:string
+    taxRuleGroups: TaxRuleGroup[]
+    sortBy: string
+    order: 'asc' | 'desc'
+}
+
+// const emit = defineEmits<{
+//   (e: 'navigate', url: string): void
+// }>() 
+
+defineProps<TaxRuleGroupsListProps>()
+</script>
+
+<template>
+    <section id="tax-rule-groups-list-wrapper" class="bg-gray-100 rounded-lg py-4 px-12">
+        <table class="flex flex-col gap-4">
+            <thead>
+                <tr class="flex flex-row gap-[5%] justify-around py-2 px-3">
+                    <th scope="col" class="m-[1rem 2rem 1rem 2rem] w-[15%] text-center">Id</th>
+                    <th scope="col" class="m-[1rem 2rem 1rem 2rem] w-[15%] text-center">Name</th>
+                    <th scope="col" class="m-[1rem 2rem 1rem 2rem] w-[15%] text-center">Rate</th>
+                    <th scope="col" class="m-[1rem 2rem 1rem 2rem] w-[15%] text-center">Applicable</th>
+                    <th scope="col" class="m-[1rem 2rem 1rem 2rem] w-[15%] text-center">Edit</th>
+                </tr>
+            </thead>
+            <tbody v-if="taxRuleGroups.length > 0" id="tax-rule-groups-list">
+                <tr v-for="taxRuleGroup in taxRuleGroups" v-bind:key="taxRuleGroup.id" class="flex flex-row gap-[5%] items-center justify-around bg-gray-200 py-2 px-3 my-4 rounded-md h-[5rem]">
+                    <th scope="row" class="m-[1rem 2rem 1rem 2rem] w-[15%] text-center">{{ taxRuleGroup.id }}</th>
+                    <td class="m-[1rem 2rem 1rem 2rem] w-[15%] text-center"><Link :href="route('admin.back-office.taxes.showRules', {slug: taxRuleGroup.slug}, false, Ziggy)"><FileEditIcon/></Link></td>
+                </tr>
+            </tbody>
+            <tbody v-else id="tax-rule-groups-list">
+                <tr>
+                    <th scope="row" class="py-2 px-3 h-[5rem]">No Tax Rules registered</th>
+                </tr>
+            </tbody>
+        </table>
+    </section>
+</template>
---
+++ resources/js/pages/admin/NewTax.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+import AppLayout from '@/layouts/AppLayout.vue';
+import AdminTaxCreateForm from '@/components/jewellery_store/form/AdminTaxCreateForm.vue';
+import { Head, Link } from '@inertiajs/vue3';
+import { showTaxes } from '@/routes/admin/back-office';
+
+interface NewTaxProps {
+}  
+
+const props = defineProps<NewTaxProps>()
+</script>
+
+
+<template>
+  <Head title='New Tax'/>
+  <AppLayout>
+    <section id="new-product-page-wrapper" class="flex flex-col mx-2">
+      <hgroup id="new-tax-page-top" class="mx-4 mt-8 mb-4">
+        <h1 id="new-tax-page-heading" class="text-xl">Add a new tax</h1>
+      </hgroup>
+      <AdminTaxCreateForm classname=""></AdminTaxCreateForm>
+      <Link :href="showTaxes()" class="w-fit inline-block rounded-sm border border-[#19140035] px-5 py-1.5 mx-6 my-8 text-sm leading-normal text-[#1b1b18] hover:border-[#1915014a] dark:border-[#3E3E3A] dark:text-[#EDEDEC] dark:hover:border-[#62605b]">
+        Back to taxes list
+      </Link>
+    </section>
+  </AppLayout>
+</template>

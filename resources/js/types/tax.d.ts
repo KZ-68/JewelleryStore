69c747914afb3ffec2e29dc335e401376b8ea73b
KZ68 <zitnik.kevin@gmail.com>
2026-01-14 16:48:09 +0100

    feat: add tax and tax rule group types

---
+++ resources/js/types/tax.d.ts
@@ -0,0 +1,10 @@
+export interface Tax {
+    id: number
+    name: string
+    rate: number
+    applicable: boolean
+    type: string
+    description: string
+    created_at: string
+    updated_at: string
+}
---
+++ resources/js/types/taxRuleGroup.d.ts
@@ -0,0 +1,5 @@
+export interface TaxRuleGroup {
+    id: number
+    name: string
+    active: boolean
+}

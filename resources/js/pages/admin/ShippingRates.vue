814d9482a122366a836e718534b7a91f21cb4435
Kevin ZITNIK <zitnik.kevin@gmail.com>
2026-01-25 23:39:44 +0100

    feat: add shipping rates view

---
+++ resources/js/pages/admin/ShippingRates.vue
@@ -0,0 +1,43 @@
+<script setup lang="ts">
+import { Country } from '@/types/country';
+import { Input } from '@/components/ui/input';
+import ShippingRateTable from '@/components/jewellery_store/table/ShippingRateTable.vue';
+import AppLayout from '@/layouts/AppLayout.vue'; 
+
+interface ShippingRatesProps {
+    countries: Country[]
+    form: {
+        name: string
+        description : string
+        min_total: number
+        max_total: number
+        price: number
+    }
+    isLast: boolean,
+} 
+
+const props = defineProps<ShippingRatesProps>()
+
+defineEmits(['prev', 'submit'])
+</script>
+
+<template>
+    <AppLayout>
+        <div id="shipping-rates-wrapper" class="flex flex-col gap-4 py-8 px-20">
+            <ShippingRateTable classname="" :countries="props.countries" :form="props.form"></ShippingRateTable>
+            <div class="flex justify-between mt-4">
+                <button @click="$emit('prev')" class="px-4 py-2 text-white bg-black rounded-md">
+                    Previous
+                </button>
+
+                <button
+                    @click="$emit('submit')"
+                    class="px-4 py-2 text-white bg-black rounded-md"
+                    :disabled="form.processing"
+                >
+                    Create
+                </button>
+            </div>
+        </div>
+    </AppLayout>
+</template>

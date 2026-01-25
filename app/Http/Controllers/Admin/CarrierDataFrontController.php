f2a699672e3a59a617396319e9c45398c453c32c
Kevin ZITNIK <zitnik.kevin@gmail.com>
2026-01-25 23:36:55 +0100

    feat: add Carrier Management Controller

---
+++ app/Http/Controllers/Admin/CarrierDataFrontController.php
@@ -0,0 +1,52 @@
+<?php
+/* 
+* Front Controller File for the carrier page connected to the Back Office section 
+*/
+
+namespace App\Http\Controllers\Admin;
+
+use App\Models\Carrier;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Models\ShippingRate;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Support\Facades\Validator;
+
+class CarrierDataFrontController extends Controller
+{
+    /**
+    * This method validate every fields used in the new shipping rate form.
+    * @param Request Get the POST method body from the form
+    * @return RedirectResponse Send a response with a redirection
+    */
+    public function carrierManagementUpdate(Request $request): RedirectResponse
+    {
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|string',
+            'description' => 'required|string',
+            'min_total' => 'required|decimal:0,2',
+            'max_total' => 'required|decimal:0,2',
+            'price' => 'required|decimal:0,2',
+            'id_carrier' => 'integer'
+        ]);
+
+        if ($validator->fails()) {
+            return redirect('/admin/back-office/carriers')
+                ->withErrors($validator)
+                ->withInput();
+        }
+
+        $carrier = Carrier::where('name', $request->get('name'))->first();
+        $carrier->name = $request->get('name');
+        $carrier->description = $request->get('description');
+        $carrier->save();
+        $shippingRate = new ShippingRate();
+        $shippingRate->min_total = $request->get('min_total');
+        $shippingRate->max_total = $request->get('max_total');
+        $shippingRate->price = $request->get('price');
+        $shippingRate->save();
+        $carrier->shippingRates()->associate($shippingRate);
+        
+        return redirect('/admin/back-office/carriers');
+    }
+}

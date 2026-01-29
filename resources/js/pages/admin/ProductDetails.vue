4edb355557a87b3cd90b227770bcb87217ce06ac
Kevin ZITNIK <zitnik.kevin@gmail.com>
2026-01-29 23:24:44 +0100

    feat: add props

--- resources/js/pages/admin/ProductDetails.vue
+++ resources/js/pages/admin/ProductDetails.vue
@@ -9,6 +9,8 @@ import { TaxRuleGroup } from '@/types/taxRuleGroup';
 interface ProductDetailsProps {
   product: Product
   taxRuleGroups: TaxRuleGroup[]
+  priceWithTax: number
+  taxRuleGroupId: number
 }  
 
 const props = defineProps<ProductDetailsProps>()
@@ -18,7 +20,7 @@ const props = defineProps<ProductDetailsProps>()
 <template>
     <Head :title=props.product.name />
     <AppLayout>
-      <AdminProductMainForm classname="" :product=props.product :taxRuleGroups=props.taxRuleGroups></AdminProductMainForm>
+      <AdminProductMainForm classname="" :product=props.product :taxRuleGroups=props.taxRuleGroups :priceWithTax=props.priceWithTax :taxRuleGroupId=props.taxRuleGroupId></AdminProductMainForm>
       <div id="product-page-footer-wrapper" class="px-6 py-8">
             <nav id="product-nav-links" class="flex flex-row gap-4">
                  <Link :href="showProducts()" class="w-fit inline-block rounded-sm border border-[#19140035] px-5 py-1.5 mx-6 my-8 text-sm leading-normal text-[#1b1b18] hover:border-[#1915014a] dark:border-[#3E3E3A] dark:text-[#EDEDEC] dark:hover:border-[#62605b]">

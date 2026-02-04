ce7d12cec738db0ed445fefb7e142e80f26ffd7c
Kevin ZITNIK <zitnik.kevin@gmail.com>
2026-02-04 16:41:09 +0100

    feat: add data and components

--- resources/js/pages/web/Cart.vue
+++ resources/js/pages/web/Cart.vue
@@ -1,12 +1,31 @@
 <script setup lang="ts">
+import { Category } from '@/types/category';
 import CartLeftColumn from '../../components/jewellery_store/cart/left_column/CartLeftColumn.vue';
+import CartSummary from '@/components/jewellery_store/CartSummary.vue';
 
+interface CartProduct {
+    name: string
+    quantity: number
+    price: number
+}
+
+interface CartProps {
+    frontCategories: Category[]
+    products: Array<CartProduct>
+}
+
+const props = defineProps<CartProps>();
 </script>
 
 <template>
-    <div
-        class="items-center min-h-screen bg-[#FDFDFC] p-6 text-[#1b1b18] lg:justify-center lg:p-8 bg-neutral-200 dark:bg-[#0a0a0a]"
-    >
-    <CartLeftColumn/>
+    <header>
+        <CategoryMenu :frontCategories="props.frontCategories"></CategoryMenu>
+    </header>
+    <div id="cart-wrapper" class="min-h-screen p-6 text-[#1b1b18] lg:p-8 bg-gray-100 dark:bg-[#0a0a0a]">
+        <div class="flex flex-row lg:justify-center gap-40">
+            <CartLeftColumn :products="props.products"/>
+            <CartSummary :products="props.products"></CartSummary>
+        </div>
     </div>
+    
 </template>

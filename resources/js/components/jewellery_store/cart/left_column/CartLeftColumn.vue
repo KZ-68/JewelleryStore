7138ec4704963e3eded64f349a7b7009aa0a590b
Kevin ZITNIK <zitnik.kevin@gmail.com>
2026-02-04 16:39:28 +0100

    feat: add interface for product cart

--- resources/js/components/jewellery_store/cart/left_column/CartLeftColumn.vue
+++ resources/js/components/jewellery_store/cart/left_column/CartLeftColumn.vue
@@ -2,13 +2,24 @@
 import CartBody from './body/CartBody.vue';
 import CartHeader from './header/CartHeader.vue';
 
+interface CartProduct {
+    name: string
+    quantity: number
+    price: number
+}
+
+interface CartLeftColumnProps {
+    products: Array<CartProduct>
+}
+
+const props = defineProps<CartLeftColumnProps>();
 </script>
 
 <template>
     <section
-        class="max-w-[900px] flex-start mt-20 ml-15 py-10 px-12 gap-1 rounded-lg bg-white p-1 dark:bg-neutral-800"
+        class="lg:w-[900px] max-w-[900px] flex-start my-10 px-8 pt-20 pb-10 gap-1 rounded-lg bg-white p-1 dark:bg-neutral-800"
     >
     <CartHeader classname="" title="Panier"/>
-    <CartBody classname="flex flex-row" name="test" :quantity=0 price="2,50" />
+    <CartBody classname="flex flex-row" :products="props.products"/>
     </section>
 </template>
--- resources/js/components/jewellery_store/cart/left_column/body/CartBody.vue
+++ resources/js/components/jewellery_store/cart/left_column/body/CartBody.vue
@@ -1,28 +1,28 @@
 <script setup lang="ts">
-interface Props {
-    classname:string;
-    name: string;
-    quantity?: number;
-    price: string;
+interface CartProduct {
+    name: string
+    quantity: number
+    price : number
 }
 
-defineProps<Props>();
+interface CartBodyProps {
+    classname:string;
+    products: Array<CartProduct>
+}
+
+const props = defineProps<CartBodyProps>();
 </script>
 
 <template>
     <div class="mb-8 space-y-0.5 flex gap-2">
-        <div class="w-[100%] bg-neutral-100 flex flex-row items-center justify-between py-8 px-6 my-3 mx-4 rounded-md">
-            <h2 class="text-xl font-semibold tracking-tight">{{ name }}</h2>
-
-            
-
+        <div v-for="product in props.products" class="w-[100%] bg-neutral-100 flex flex-row items-center justify-between py-8 px-6 my-3 mx-4 rounded-md">
+            <h2 class="text-xl font-semibold tracking-tight">{{ product.name }}</h2>
             <aside class="flex flex-row text-sm text-muted-foreground gap-20">
                 <span>
-                ({{price}})
+                ({{product.price}})
                 </span>
 
-                <p v-if="quantity">({{quantity}})</p>
-                <p v-else>(0)</p>
+                <p>({{product.quantity}})</p>
             </aside>
         </div>
     </div>

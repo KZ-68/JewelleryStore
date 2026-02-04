900da620b52709f23e8442c9ce56916e5575002e
Kevin ZITNIK <zitnik.kevin@gmail.com>
2026-02-04 16:41:25 +0100

    feat: add routes for cart

--- routes/web.php
+++ routes/web.php
@@ -23,6 +23,7 @@ Route::get('/', function () {
 })->name('home');
 
 Route::get('/cart', [CartController::class, 'show'])->name('cart.show');
+Route::post('/cart/add', [CartController::class, 'addToCart'])->name('cart.addToCart');
 Route::get('contact', [ContactController::class, 'create'])->name('contact.create');
 Route::post('contact', [ContactController::class, 'store'])->name('contact.store');
 

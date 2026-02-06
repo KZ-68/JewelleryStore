11752ca708ec1a6946722c25228e4b3a627692f8
Kevin ZITNIK <zitnik.kevin@gmail.com>
2026-02-06 16:35:15 +0100

    feat: add Cart Notifier component

---
+++ resources/js/components/jewellery_store/cart/notifier/CartNotifier.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+import { show } from '@/routes/cart'
+import { HandbagIcon } from 'lucide-vue-next';
+import { Link } from '@inertiajs/vue3';
+import { toRefs, computed } from 'vue';
+
+interface CartNotifierProps {
+    cartProductsCount: number
+}
+
+const props = defineProps<CartNotifierProps>();
+
+const shopHeader = toRefs(props);
+const cartNumber = computed(() => shopHeader.cartProductsCount)
+</script>
+
+<template>
+    <div id="cart-notifier-wrapper">
+        <Link id="cart-notifier-anchor" class="relative flex flex-row items-center gap-2" :href="show()">
+            <div id="cart-notifier-body">
+                <HandbagIcon width="100px"/>
+            </div>
+            <div id="cart-notifier-header">
+                <span class="flex flex-row justify-center items-center w-[20px] h-[20px] py-0.5 px-2.5 z-10 left-4 rounded-full bg-gray-200">
+                    <div>
+                        {{ cartNumber }}
+                    </div>
+                </span>
+            </div>
+        </Link>
+    </div>
+</template>

5ee25da6b192b7acbcde895485f0081d59358528
Kevin ZITNIK <zitnik.kevin@gmail.com>
2026-02-06 16:34:57 +0100

    feat: add Top Products Carousel component

---
+++ resources/js/components/jewellery_store/carousel/TopProductCarousel.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+import { Swiper, SwiperSlide } from "swiper/vue";
+import "swiper/css";
+import "swiper/css/navigation";
+import { Navigation } from 'swiper/modules';;
+import { Product } from "@/types/product";
+import ProductCard from "../card/ProductCard.vue";
+
+interface TopProductCarouselProps {
+    topProducts: Product[]
+}
+
+const props = defineProps<TopProductCarouselProps>();
+</script>
+
+<template>
+    <section id="top-product-carousel-section" class="w-7xl">
+        <div id="top-product-carousel-wrapper">
+            <hgroup id="top-product-carousel-heading">
+                <h2 class="px-3 py-14 font-bold text-3xl">Top Products</h2>
+            </hgroup>
+            <div class="pl-6">
+                <swiper :navigation="true" :modules="[Navigation]" :slides-per-view="4" :space-between="10" class="mySwiper">
+                    <swiper-slide v-for="topProduct in props.topProducts">
+                        <ProductCard classname="" :product="topProduct" :image="null"></ProductCard>
+                    </swiper-slide>
+                </swiper>
+            </div>
+        </div>
+    </section>
+</template>

53ea476ac314d98bb85623bcf5dd42c21d9c9e6f
Kevin ZITNIK <zitnik.kevin@gmail.com>
2026-02-06 16:35:52 +0100

    refactor: update home page route for the new controller

--- routes/web.php
+++ routes/web.php
@@ -4,23 +4,21 @@ use Inertia\Inertia;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Web\CartController;
 use App\Http\Controllers\Web\ContactController;
+use App\Http\Controllers\Web\HomeFrontController;
 use App\Http\Controllers\Admin\TaxFrontController;
+use App\Http\Controllers\Web\OrderFrontController;
 use App\Http\Controllers\Admin\BackOfficeController;
-use App\Http\Controllers\Admin\CarrierDataFrontController;
 use App\Http\Controllers\Admin\CarrierFrontController;
 use App\Http\Controllers\Admin\ProductFrontController;
 use App\Http\Controllers\Admin\CategoryFrontController;
 use App\Http\Controllers\Admin\CustomerFrontController;
 use App\Http\Controllers\Admin\SupplierFrontController;
-use App\Http\Controllers\Admin\ManufacturerFrontController;
-use App\Http\Controllers\Admin\ShippingRateFrontController;
-use App\Http\Controllers\Admin\TaxRuleGroupFrontController;
-use App\Http\Controllers\Web\OrderFrontController;
 use App\Http\Controllers\Web\ShopProductFrontController;
+use App\Http\Controllers\Admin\CarrierDataFrontController;
+use App\Http\Controllers\Admin\ManufacturerFrontController;
+use App\Http\Controllers\Admin\TaxRuleGroupFrontController;
 
-Route::get('/', function () {
-    return Inertia::render('Home');
-})->name('home');
+Route::get('/', [HomeFrontController::class, 'show'])->name('home');
 
 Route::get('/cart', [CartController::class, 'show'])->name('cart.show');
 Route::post('/cart/add', [CartController::class, 'addToCart'])->name('cart.addToCart');

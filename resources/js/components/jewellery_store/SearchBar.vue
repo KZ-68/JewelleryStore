a21d1e45b8a58d3b2ff5dcb6560ec5f044c533dc
Kevin ZITNIK <zitnik.kevin@gmail.com>
2026-02-13 16:37:28 +0100

    feat: add search bar component

---
+++ resources/js/components/jewellery_store/SearchBar.vue
@@ -0,0 +1,48 @@
+<script setup lang="ts">
+import axios from 'axios'
+import { Product } from '@/types/product';
+import { onMounted, ref } from "vue";
+import { route } from '../../../../vendor/tightenco/ziggy/src/js';
+import { Ziggy } from '../../ziggy.js';import SearchProductCard from './card/SearchProductCard.vue';
+;
+
+let searchText = ref('');
+let results = ref<Product[]|null>(null);
+let searchBarActive = ref(false);
+
+
+async function search(text: string) {
+    searchBarActive.value = true;
+    if(text === '') {
+        results.value = null;
+        searchBarActive.value = false;
+    }
+
+    try {
+        await axios.post(
+            route('searchProducts', {text: text}, false, Ziggy), {text: text}
+        ).then(response => {
+            results.value = response.data.results
+        })
+    } catch (error) {
+        console.error('Search Error :', error);
+    }
+}
+
+</script>
+
+<template>
+    <div id="search-bar-wrapper" class="relative mx-8">
+        <input @input="search(searchText)" id="shop-search-bar" type="text" name="text" v-model="searchText" class="w-3xl px-2 py-3 rounded-md border-2 border-gray-200">
+        <div id="search-results-wrapper" class="absolute px-3 py-4">
+            <ul id="search-results-list" class="flex flex-col">
+                <li v-if="results !== null" v-for="product in results">
+                    <SearchProductCard :product="product"></SearchProductCard>
+                </li>
+                <li v-else-if="searchBarActive === true && results === null" class="flex flex-row">
+                    <p>No products found</p>
+                </li>
+            </ul>
+        </div>
+    </div>
+</template>

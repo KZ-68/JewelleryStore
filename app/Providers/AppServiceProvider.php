ffec353ab4c8eccb36045d5c6b3d5a362429ea60
Kevin ZITNIK <zitnik.kevin@gmail.com>
2026-02-13 16:35:28 +0100

    feat: register in singleton Product Image Service

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,8 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use App\Contracts\ProductImageServiceInterface;
+use App\Services\Image\ProductImageService;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -11,7 +13,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        $this->app->singleton(
+            ProductImageServiceInterface::class,
+            ProductImageService::class
+        );
     }
 
     /**

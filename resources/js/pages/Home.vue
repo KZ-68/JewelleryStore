7909fe6965b849b59b8f5f52e111db2f470af098
Kevin ZITNIK <zitnik.kevin@gmail.com>
2026-02-18 23:54:34 +0100

    feat: add Slider and ShopFooter

--- resources/js/pages/Home.vue
+++ resources/js/pages/Home.vue
@@ -4,6 +4,8 @@ import { Category } from '@/types/category';
 import { Product } from '@/types/product';
 import TopProductCarousel from '@/components/jewellery_store/carousel/TopProductCarousel.vue';
 import ShopHeader from '@/components/jewellery_store/ShopHeader.vue';
+import Slider from '@/components/jewellery_store/Slider.vue';
+import ShopFooter from '@/components/jewellery_store/ShopFooter.vue';
 
 interface HomeProps {
     frontCategories: Category[]
@@ -19,15 +21,15 @@ const props = defineProps<HomeProps>()
         <link rel="preconnect" href="https://rsms.me/" />
         <link rel="stylesheet" href="https://rsms.me/inter/inter.css" />
     </Head>
-    <div
-        class="flex min-h-screen flex-col bg-[#FDFDFC] text-[#1b1b18] dark:bg-[#0a0a0a]"
-    >
+    <div class="flex min-h-screen flex-col bg-[#FDFDFC] text-[#1b1b18] dark:bg-[#0a0a0a]">
         <ShopHeader :frontCategories="props.frontCategories" :cartProductsCount="props.cartProductsCount"></ShopHeader>
         <main
-            class="flex w-full items-center justify-center opacity-100 transition-opacity duration-750 lg:grow starting:opacity-0"
+            class="flex flex-col w-full items-center justify-center opacity-100 transition-opacity duration-750 lg:grow starting:opacity-0"
         >
+            <Slider></Slider>
             <TopProductCarousel :topProducts="props.topProducts"></TopProductCarousel>
         </main>
         <div class="hidden h-14.5 lg:block"></div>
+        <ShopFooter></ShopFooter>
     </div>
 </template>

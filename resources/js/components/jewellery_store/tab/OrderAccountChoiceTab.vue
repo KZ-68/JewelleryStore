fc2f8f684d16210a9e3b696647bd99e345e84dbf
Kevin ZITNIK <zitnik.kevin@gmail.com>
2026-02-20 16:32:18 +0100

    feat: add conditions for activate click event

--- resources/js/components/jewellery_store/tab/OrderAccountChoiceTab.vue
+++ resources/js/components/jewellery_store/tab/OrderAccountChoiceTab.vue
@@ -4,8 +4,8 @@ import { ref } from 'vue'
 import OrderAccountRegisterForm from '../form/OrderAccountRegisterForm.vue';
 
 interface OrderAccountFormProps {
-    classname:string
     status?: string;
+    isAddressSelected: boolean
 }   
 const isLogin = ref(false);
 const isRegister = ref(false);
@@ -25,16 +25,28 @@ const props = defineProps<OrderAccountFormProps>();
 
 <template>
     <section id="order-account-choice-wrapper" class="py-8">
-        <div id="order-account-choice" class="my-2 mx-4 w-[700px] max-w-[700px] gap-1">
+        <div id="order-account-choice" class="my-2 mx-4 w-[60rem] max-w-[60rem] gap-1">
             <div id="account-choice-tab-button" class="bg-white flex flex-col justify-center px-3 py-6 rounded-md">
                 <h2 class="text-xl  mx-3 my-2">1. Connecting an account</h2>
-                <div class="flex flex-row justify-center my-4">
-                    <button @click="selectLogin()" class="bg-[#84070F] px-3 py-2 mx-4 text-white font-bold rounded-sm">Login Account</button>
-                    <button @click="selectRegister()" class="bg-[#84070F] px-3 py-2 mx-4 text-white font-bold rounded-sm">Register Account</button>
+                <div v-if="!$page.props.auth.customer" class="flex flex-row justify-center my-4">
+                    <button @click="selectLogin()" class="bg-[#84070F] px-3 py-2 mx-4 text-white font-bold rounded-md hover:cursor-pointer hover:bg-[#a32a32]">Login Account</button>
+                    <button @click="selectRegister()" class="bg-[#84070F] px-3 py-2 mx-4 text-white font-bold rounded-md hover:cursor-pointer hover:bg-[#a32a32]">Register Account</button>
                 </div>
             </div>
             <div v-if="isLogin === true"><OrderAccountLoginForm classname="" :status="props.status"></OrderAccountLoginForm></div>
             <div v-if="isRegister === true"><OrderAccountRegisterForm></OrderAccountRegisterForm></div>
+            <button v-if="$page.props.auth.customer" @click="$emit('selectStep', 1)" id="order-address-step-tab" class="bg-white py-6 my-2 w-[60rem] max-w-[60rem] rounded-t-md hover:cursor-pointer hover:bg-gray-50">
+                <h2 class="text-left text-xl px-3 py-4">2. Delivery address</h2>
+            </button>
+            <button v-else id="order-address-step-tab" class="bg-white py-6 my-2 w-[60rem] max-w-[60rem] rounded-t-md">
+                <h2 class="text-left text-xl px-3 py-4">2. Delivery address</h2>
+            </button>
+            <button v-if="isAddressSelected === true" @click="$emit('selectStep', 2)" id="order-address-step-tab" class="bg-white py-6 my-2 w-[60rem] max-w-[60rem] rounded-t-md hover:cursor-pointer :hover:hover:bg-gray-50">
+                <h2 class="text-left text-xl px-3 py-4">3. Select a carrier</h2>
+            </button>
+            <button v-else id="order-address-step-tab" class="bg-white py-6 my-2 w-[60rem] max-w-[60rem] rounded-t-md">
+                <h2 class="text-left text-xl px-3 py-4">3. Select a carrier</h2>
+            </button>
         </div>
     </section>
 </template>

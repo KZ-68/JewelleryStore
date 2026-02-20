7a2dbb3fbc45c221b6dffd75c85383fd99e1bbaf
Kevin ZITNIK <zitnik.kevin@gmail.com>
2026-02-20 16:44:05 +0100

    style: update responsive properties

--- resources/js/components/jewellery_store/list/addresses/AddressesList.vue
+++ resources/js/components/jewellery_store/list/addresses/AddressesList.vue
@@ -17,7 +17,7 @@ const props = defineProps<AddressesListProps>()
 
 <template>
     <section id="addresses-list-wrapper" class="h-[36rem] rounded-lg">
-        <ul v-if="addresses.length > 0" id="addresses-list" class="flex flex-row flex-wrap gap-2 mx-4">
+        <ul v-if="addresses.length > 0" id="addresses-list" class="flex flex-col md:flex-row flex-wrap gap-2 mx-4">
             <li v-for="address in addresses" v-bind:key="address.id" class="bg-white rounded-md my-3">
                 <AddressCard classname="" :address="address" :isOrder="props.isOrder"/>
             </li>

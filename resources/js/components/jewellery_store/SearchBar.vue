e4792437891c1fe205a30ec769d5b2df4c06ffa8
Kevin ZITNIK <zitnik.kevin@gmail.com>
2026-02-27 16:45:37 +0100

    style: update result position for mobile

--- resources/js/components/jewellery_store/SearchBar.vue
+++ resources/js/components/jewellery_store/SearchBar.vue
@@ -4,12 +4,10 @@ import { Product } from '@/types/product';
 import { onMounted, ref } from "vue";
 import { route } from '../../../../vendor/tightenco/ziggy/src/js';
 import { Ziggy } from '../../ziggy.js';import SearchProductCard from './card/SearchProductCard.vue';
-;
 
 let searchText = ref('');
 let results = ref<Product[]|null>(null);
 let searchBarActive = ref(false);
-
 
 async function search(text: string) {
     searchBarActive.value = true;
@@ -34,7 +32,7 @@ async function search(text: string) {
 <template>
     <div id="search-bar-wrapper" class="flex flex-col lg:block relative mx-4 lg:mx-8">
         <input @input="search(searchText)" id="shop-search-bar" type="text" name="text" v-model="searchText" class="w-80 lg:w-3xl px-2 py-3 rounded-md border-2 border-gray-200">
-        <div id="search-results-wrapper" class="absolute px-3 py-4 z-[2]">
+        <div id="search-results-wrapper" class="absolute top-9 lg:top-0 px-3 py-4 z-[2]">
             <ul id="search-results-list" class="flex flex-col">
                 <li v-if="results !== null" v-for="product in results">
                     <SearchProductCard :product="product" :image="product.image"></SearchProductCard>

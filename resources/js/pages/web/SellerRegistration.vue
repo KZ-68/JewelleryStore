0bc59b49019ce87ede97c853b244fe2c91979e8d
Kevin ZITNIK <zitnik.kevin@gmail.com>
2026-03-05 23:58:31 +0100

    feat: add SellerRegister Page view

---
+++ resources/js/pages/web/SellerRegistration.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import SellerRegisterForm from '@/components/jewellery_store/form/SellerRegisterForm.vue';
+
+interface SellerRegistrationProps {
+    classname:string
+    status?: string;
+}   
+
+const props = defineProps<SellerRegistrationProps>();
+</script>
+
+<template>
+    <section id="seller-registration-section">
+        <SellerRegisterForm></SellerRegisterForm>
+    </section>
+</template>

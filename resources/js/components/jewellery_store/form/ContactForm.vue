8e2cd3558a89ba8859d153d97f085c1349c1f3a2
Kevin ZITNIK <zitnik.kevin@gmail.com>
2026-03-08 00:00:49 +0100

    feat: add responsive burger menu

--- resources/js/components/jewellery_store/form/ContactForm.vue
+++ resources/js/components/jewellery_store/form/ContactForm.vue
@@ -15,7 +15,7 @@ defineProps<ContactFormProps>();
 </script>
 
 <template>
-    <section id="contact-form-wrapper" class="my-10 mx-15 max-w-[900px] flex-start py-6 px-8 gap-1 rounded-lg bg-white p-1 dark:bg-neutral-800">
+    <section id="contact-form-wrapper" class="lg:my-10 lg:mx-15 w-[300px] lg:w-[900px] max-w-[300px] lg:max-w-[900px] py-6 px-8 gap-1 rounded-lg bg-white p-1 dark:bg-neutral-800">
         <Form
             v-bind="ContactController.store.form()"
             :reset-on-success="['contact']"
@@ -24,7 +24,7 @@ defineProps<ContactFormProps>();
         >
             <div class="grid gap-6">
                 <div class="grid gap-2">
-                    <Label for="email">Email address</Label>
+                    <Label for="email" class="text-lg">Email address</Label>
                     <Input
                         id="email"
                         type="email"
@@ -34,13 +34,13 @@ defineProps<ContactFormProps>();
                         :tabindex="1"
                         autocomplete="email"
                         placeholder="email@example.com"
-                        class="bg-gray-100 p-1 rounded-md"
+                        class="bg-gray-100 p-1 rounded-md h-12 text-md"
                     />
                     <InputError :message="errors.email" />
                 </div>
 
                 <div class="grid gap-2">
-                    <Label for="subject">Subject</Label>
+                    <Label for="subject" class="text-lg">Subject</Label>
                     <Input
                         id="subject"
                         type="text"
@@ -49,13 +49,13 @@ defineProps<ContactFormProps>();
                         autofocus
                         :tabindex="2"
                         placeholder="Type your subject here"
-                        class="bg-gray-100 p-1"
+                        class="bg-gray-100 p-1 h-12 text-md"
                     />
                     <InputError :message="errors.subject" />
                 </div>
 
                 <div class="grid gap-2">
-                    <Label for="text">Message</Label>
+                    <Label for="text" class="text-lg">Message</Label>
                     <textarea
                         id="contact-message"
                         name="message"
@@ -67,13 +67,13 @@ defineProps<ContactFormProps>();
                         autofocus
                         :tabindex="3"
                         placeholder="Type your message here"
-                        class="bg-gray-100 p-1 rounded-md"
+                        class="bg-gray-100 p-1 rounded-md text-md"
                     />
                     <InputError :message="errors.subject" />
                 </div>
 
                 <div class="flex items-center justify-between">
-                    <Label for="agreed" class="flex items-center space-x-3">
+                    <Label for="agreed" class="flex items-center space-x-3 text-md">
                         <Checkbox id="agreed" name="agreed" :tabindex="4" />
                         <span>
                             By submitting this form, I declare that I have read the privacy policy and authorize the Controller to respond to me as expressed in point a and b of the
@@ -90,7 +90,7 @@ defineProps<ContactFormProps>();
 
                 <Button
                     type="submit"
-                    class="mt-4 w-20 bg-black text-white"
+                    class="mt-4 w-20 py-2 bg-[#84070F] hover:bg-red-800 hover:cursor-pointer font-bold text-white rounded-sm"
                     :tabindex="5"
                     :disabled="processing"
                     data-test="login-button"
--- resources/js/pages/web/ShopCategoryProducts.vue
+++ resources/js/pages/web/ShopCategoryProducts.vue
@@ -3,14 +3,17 @@ import type { Product } from '@/types/product'
 import ProductCard from '@/components/jewellery_store/card/ProductCard.vue';
 import { Category } from '@/types/category';
 import { router } from '@inertiajs/vue3'
-import { ref } from "vue";
+import { provide, ref } from "vue";
 import { route } from '../../../../vendor/tightenco/ziggy/src/js';
 import { Ziggy } from '../../ziggy.js';
 import ShopHeader from '@/components/jewellery_store/ShopHeader.vue';
+import BurgerMenu from '@/components/jewellery_store/nav/mobile/BurgerMenu.vue';
+import { useWindowSize } from '@vueuse/core';
 
 interface ProductsListProps {
     classname:string
     category_slug: string
+    category_name: string
     products: Product[]
     frontCategories: Category[]
     filters: {
@@ -37,12 +40,26 @@ const updateCustomersFilters = () => {
     order: order.value,
   });
 }
+
+const { width } = useWindowSize()
+const active = ref<boolean>(false)
+provide('active', active)
+
+const openNav = () => {
+  active.value = true
+}
 </script>
 
 <template>
-    <ShopHeader :frontCategories="props.frontCategories" :cartProductsCount="props.cartProductsCount"></ShopHeader>
+    <button v-if="width <= 430" id="openBtn" @click="openNav" class="absolute top-0 left-0 flex flex-col gap-1 p-4 bg-white z-[2]">
+        <div class="w-[20px] h-0.5 bg-[#84070F]"></div>
+        <div class="w-[20px] h-0.5 bg-[#84070F]"></div>
+        <div class="w-[20px] h-0.5 bg-[#84070F]"></div>
+    </button>
+    <ShopHeader v-if="width > 430" :frontCategories="props.frontCategories" :cartProductsCount="props.cartProductsCount"></ShopHeader>
+    <BurgerMenu v-else :frontCategories="props.frontCategories" :cartProductsCount="props.cartProductsCount" :active="active"></BurgerMenu>
     <main class="flex flex-col h-fit gap-8 px-24 py-8 bg-gray-100">
-        <h1 class="my-4 text-3xl">Products Category</h1>
+        <h1 class="my-4 text-3xl">Products for {{ category_name }} Category</h1>
         <div id="products-category-body" class="flex flex-row gap-48">
             <section class="flex flex-col flex-wrap gap-3 px-3 py-2 rounded-xl bg-white">
                 <div id="category-products-filters-wrapper" class="flex flex-col my-6 gap-4">

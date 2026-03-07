06c741f79f70ea66c32e61b2dc72a03c4c201f78
Kevin ZITNIK <zitnik.kevin@gmail.com>
2026-03-07 20:03:21 +0100

    feat: add sub categories component

---
+++ resources/js/components/jewellery_store/nav/SubCategory.vue
@@ -0,0 +1,45 @@
+<script setup lang="ts">
+import { Category } from '@/types/category';
+import { route } from '../../../../../vendor/tightenco/ziggy';
+import { Ziggy } from '../../../ziggy.js';
+
+interface SubCategoryMenuProps {
+    category: Category
+    openCategories: Set<number>
+}
+
+const props = defineProps<SubCategoryMenuProps>();
+
+const emit = defineEmits<{
+  (e: 'open', id: number): void
+  (e: 'close', id: number): void
+}>()
+</script>
+
+<template>
+    <li @mouseenter="emit('open', props.category.id)" @touchstart="emit('open', props.category.id)" @click="emit('close', props.category.id)" class="flex flex-row relative group">
+        <div class="flex items-center justify-between px-4 py-3 hover:bg-gray-50 transition-colors border-b-2 border-b-gray-100">
+            <a :href="route('showCategoryProducts', {category_slug: props.category.slug}, false, Ziggy)" class="flex-1 font-medium text-gray-800 hover:text-blue-500 transition-colors">
+            {{ props.category.name }}
+            </a>
+            <svg
+                class="w-5 h-5 transition-transform duration-300"
+                viewBox="0 0 20 20"
+                :class="{ 'rotate-270': openCategories.has(category.id) }"
+                fill="currentColor"
+                >
+                <path d="M6 8l4 4 4-4" stroke="currentColor" stroke-width="2" fill="none"/>
+            </svg>
+        </div>
+        <ul v-if="props.openCategories.has(category.id) && props.category.children_recursive?.length" class="absolute left-0 top-full flex-col bg-white border shadow-md min-w-[200px] group-hover:flex">
+            <SubCategory
+                v-for="child in props.category.children_recursive"
+                :key="child.id"
+                :category="child"
+                :open-categories="props.openCategories"
+                @open="$emit('open', $event)"
+                @close="$emit('close', $event)"
+            />
+        </ul>
+    </li>
+</template>

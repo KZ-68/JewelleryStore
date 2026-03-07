637a60792e33a6d0e5b81ca9389cdaebd6144c2c
Kevin ZITNIK <zitnik.kevin@gmail.com>
2026-03-07 23:58:29 +0100

    feat: add product image active event

--- resources/js/pages/web/ShopProductPage.vue
+++ resources/js/pages/web/ShopProductPage.vue
@@ -7,10 +7,12 @@ import ShopHeader from '@/components/jewellery_store/ShopHeader.vue'
 import { Swiper, SwiperSlide } from "swiper/vue";
 import "swiper/css";
 import "swiper/css/navigation";
-import { onMounted, ref } from "vue";
+import { provide, ref } from "vue";
 import { route } from '../../../../vendor/tightenco/ziggy/src/js';
 import { Ziggy } from '../../ziggy.js';
 import ShopFooter from '@/components/jewellery_store/ShopFooter.vue';
+import { useWindowSize } from '@vueuse/core';
+import BurgerMenu from '@/components/jewellery_store/nav/mobile/BurgerMenu.vue';
 
 interface ProductsListProps {
     classname:string
@@ -38,10 +40,29 @@ async function addToCart(product:Product, quantity: number, retailPrice: number)
         console.error('Erreur:', error);
     }
 }
+const { width } = useWindowSize()
+const active = ref<boolean>(false)
+provide('active', active)
+
+const openNav = () => {
+  active.value = true
+}
+
+const activeIndex = ref(0)
+
+function onSlideChange(swiper: any) {
+  activeIndex.value = swiper.activeIndex
+}
 </script>
 
 <template>
-    <ShopHeader :frontCategories="props.frontCategories" :cartProductsCount="props.cartProductsCount"></ShopHeader>
+    <button v-if="width <= 430" id="openBtn" @click="openNav" class="absolute top-0 left-0 flex flex-col gap-1 p-4 bg-white z-[2]">
+        <div class="w-[20px] h-0.5 bg-[#84070F]"></div>
+        <div class="w-[20px] h-0.5 bg-[#84070F]"></div>
+        <div class="w-[20px] h-0.5 bg-[#84070F]"></div>
+    </button>
+    <ShopHeader v-if="width > 430" :frontCategories="props.frontCategories" :cartProductsCount="props.cartProductsCount"></ShopHeader>
+    <BurgerMenu v-else :frontCategories="props.frontCategories" :cartProductsCount="props.cartProductsCount" :active="active"></BurgerMenu>
     <main id="shop-product-page-main" class="px-24 py-8 bg-gray-100">
         <section id="shop-product-page-main-wrapper">
             <div id="shop-product-page-header" class="flex flex-col gap-2 my-4 mx-2">
@@ -52,13 +73,13 @@ async function addToCart(product:Product, quantity: number, retailPrice: number)
                     <div class="flex flex-row flex-nowrap justify-evenly mx-4">
                         <div id="shop-product-page-images-wrapper" class="relative w-xl flex flex-col gap-8 pl-16 pr-8">
                             <div class="w-[450px] h-[450px] mb-4">
-                                <div class="w-[450px] h-[450px] rounded-lg bg-white mb-4 flex items-center justify-center">
-                                    <span class="text-5xl">1</span>
-                                </div>
+                                <figure class="w-[450px] h-[450px] rounded-lg bg-white mb-4 flex items-center justify-center">
+                                    <img :src="props.productImages[activeIndex]" alt="">
+                                </figure>
                             </div>
                             
                             <div id="shop-product-page-images-swipper-wrapper" class="flex flex-row justify-around">
-                                <swiper :navigation="true" :modules="[Navigation]" :slides-per-view="3" :space-between="1" id="mySwiper">
+                                <swiper :navigation="true" :modules="[Navigation]" @slideChange="onSlideChange" :slides-per-view="3" :space-between="1" id="mySwiper">
                                     <swiper-slide v-for="image in productImages" :key=image>
                                         <figure class="w-[128px] lg:h-[128px] rounded-lg bg-white mb-4 flex items-center justify-center">
                                             <img :src="image" class="w-[128px] lg:h-[128px] rounded-lg" />

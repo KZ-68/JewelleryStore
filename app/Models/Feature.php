1b993f3b0034ecd407b665263d715e934e5a6cb8
Kevin ZITNIK <zitnik.kevin@gmail.com>
2026-03-08 01:02:00 +0100

    feat: add Feature model

---
+++ app/Models/Feature.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+
+class Feature extends Model
+{
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var list<string>
+     */
+    protected $fillable = [
+        'name',
+    ];
+
+    /**
+    * The products that belong to the feature.
+    */
+    public function products(): BelongsToMany
+    {
+        return $this->belongsToMany(Product::class, 'feature_values', 'feature_id', 'product_id');
+    }
+
+    /**
+     * Get the attributes that should be cast.
+     *
+     * @return array<string, string>
+     */
+    protected function casts(): array
+    {
+        return [
+            'created_at' => 'datetime:Y-m-d H:i:s',
+            'updated_at' => 'datetime:Y-m-d H:i:s',
+        ];
+    }
+}

d3488eef6632d434e4e57ebfd27f8d09dcda49fe
Kevin ZITNIK <zitnik.kevin@gmail.com>
2026-03-09 21:24:31 +0100

    feat: add messages models migration

---
+++ database/migrations/2026_03_09_193655_create_seller_messages_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('messages', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('from_email', 200);
+            $table->string('from_name', 100);
+            $table->string('to_email', 200);
+            $table->string('to_name', 100);
+            $table->string('subject', 500);
+            $table->text('content');
+            $table->timestamp('created_at')->useCurrent();
+            $table->dateTime('read_at')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('messages');
+    }
+};

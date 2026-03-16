9d37706418607a6a236e43aa40b63e6d8956b406
Kevin ZITNIK <zitnik.kevin@gmail.com>
2026-03-16 23:44:43 +0100

    feat: add MessagesMenu component

---
+++ resources/js/components/jewellery_store/message/MessagesMenu.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+
+defineEmits(['selectChoice']);
+</script>
+
+<template>
+    <div class="message-menu-wrapper">
+        <ul class="flex flex-col lg:flex-row gap-4 text-xl">
+            <li><button class="cursor-pointer">Home</button></li>
+            <li><button class="cursor-pointer" @click="$emit('selectChoice', 0)">Received Box</button></li>
+            <li><button class="cursor-pointer" @click="$emit('selectChoice', 1)">Sent Box</button></li>
+        </ul>
+    </div>
+</template>

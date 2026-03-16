d2611b0f8abe45e10e6b8236e752518a47ef36b9
Kevin ZITNIK <zitnik.kevin@gmail.com>
2026-03-16 23:45:10 +0100

    feat: add buttons for selecting all products for seller or message box

--- resources/js/pages/settings/SellerPage.vue
+++ resources/js/pages/settings/SellerPage.vue
@@ -7,6 +7,9 @@ import { useWindowSize } from '@vueuse/core';
 import { provide, ref } from 'vue';
 import ProductsList from '@/components/jewellery_store/list/products/ProductsList.vue';
 import SettingsLayout from '@/layouts/settings/Layout.vue';
+import ShopFooter from '@/components/jewellery_store/ShopFooter.vue';
+import { Ziggy } from '@/ziggy';
+import { route } from '../../../../vendor/tightenco/ziggy/src/js';
 
 interface SellerPage {
     classname:string
@@ -29,6 +32,16 @@ provide('active', active)
 const openNav = () => {
   active.value = true
 }
+
+const sellerProductsSelected = ref(false);
+function selectSellerProducts() {
+    sellerProductsSelected.value = true;
+}
+
+function deselectSellerProducts() {
+    sellerProductsSelected.value = false;
+}
+
 </script>
 
 <template>
@@ -41,18 +54,24 @@ const openNav = () => {
     <BurgerMenu v-else :frontCategories="props.frontCategories" :cartProductsCount="props.cartProductsCount" :active="active"></BurgerMenu>
     <main>
         <SettingsLayout>
-            <div class="w-[80rem] mx-auto my-auto">
-                <nav>
-                    <ul class="flex flex-row gap-0">
-                        <li class="py-2 px-3">Products</li>
-                    </ul>
+            <div id="seller-page-wrapper" class="my-14 mx-24">
+                <nav class="flex flex-row flex-wrap gap-6 w-3xl">
+                    <button v-if="sellerProductsSelected === false" @click="selectSellerProducts" class="py-20 px-24 border-4 border-[#84070F] rounded-2xl text-2xl cursor-pointer hover:bg-amber-100 font-bold">All Products</button>
+                    <a :href="route('seller.messagesBox', {}, false, Ziggy)" class="py-20 px-24 border-4 border-[#84070F] rounded-2xl text-2xl cursor-pointer hover:bg-amber-100 font-bold">Messages Box</a>
                 </nav>
-                <div class="flex flex-col w-full h-auto mt-[3%]">
-                    <ProductsList classname="" :products="props.products" :sort-by="sortBy" :order="props.order"></ProductsList>
-                </div>
+                <section v-if="sellerProductsSelected === true" class="w-[80rem] mx-auto my-10">
+                    <nav>
+                        <ul class="flex flex-row gap-0">
+                            <li class="py-2 px-3">Products</li>
+                        </ul>
+                    </nav>
+                    <div class="flex flex-col w-full h-auto mt-[3%]">
+                        <ProductsList classname="" :products="props.products" :sort-by="sortBy" :order="props.order"></ProductsList>
+                    </div>
+                </section>
+                <button v-if="sellerProductsSelected === true" @click="deselectSellerProducts()" class="py-2 px-6 text-white bg-[#84070F] rounded-md cursor-pointer hover:bg-red-800">Back</button>
             </div>
         </SettingsLayout>
-        
     </main>
     <ShopFooter></ShopFooter>
 </template>

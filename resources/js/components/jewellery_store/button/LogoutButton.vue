7ff411311e4de67c1e8f9e1800fe09c900343085
Kevin ZITNIK <zitnik.kevin@gmail.com>
2026-03-21 11:06:09 +0100

    feat: update logout route if it's user or customer

--- resources/js/components/jewellery_store/button/LogoutButton.vue
+++ resources/js/components/jewellery_store/button/LogoutButton.vue
@@ -4,6 +4,8 @@ import { cn } from '@/lib/utils'
 import { Primitive, type PrimitiveProps } from 'reka-ui'
 import { type ButtonVariants, buttonVariants } from '.'
 import axios from 'axios';
+import { route } from '../../../../../vendor/tightenco/ziggy/src/js';
+import { Ziggy } from '@/ziggy';
 
 interface Props extends PrimitiveProps {
   variant?: ButtonVariants['variant']
@@ -15,11 +17,19 @@ const props = withDefaults(defineProps<Props>(), {
   as: 'button',
 })
 
-function submit() {
-    axios.post('/logout')
+function submit(guard: string) {
+    if (guard === 'web') {
+      axios.post('/logout')
         .then((res) => {
         console.log(res.data)
-    })
+      })
+    } else {
+      axios.post(route('admin-logout', {}, false, Ziggy))
+        .then((res) => {
+        console.log(res.data)
+      })
+    }
+
 }
 </script>
 
@@ -29,7 +39,7 @@ function submit() {
     :as="as"
     :as-child="asChild"
     :class="cn(buttonVariants({ variant, size }), props.class)"
-    @click="submit"
+    @click="$page.props.auth.user ? submit('admin') : submit('web')"
   >
     <slot />
   </Primitive>

fa8792e2faa281a56fff0e6dc14105e4bbecfb09
Kevin ZITNIK <zitnik.kevin@gmail.com>
2026-03-21 11:05:36 +0100

    style: update form properties, add image section

--- resources/js/pages/web/SellerRegistration.vue
+++ resources/js/pages/web/SellerRegistration.vue
@@ -10,7 +10,14 @@ const props = defineProps<SellerRegistrationProps>();
 </script>
 
 <template>
-    <section id="seller-registration-section">
-        <SellerRegisterForm></SellerRegisterForm>
-    </section>
+    <main class="flex flex-row bg-gray-100 rounded-xl border-t-[1px] border-gray-100">
+        <section id="seller-registration-section" class="lg:ml-10 lg:my-10 lg:px-10 lg:py-10 rounded-l-xl lg:w-50/50 bg-white">
+            <SellerRegisterForm></SellerRegisterForm>
+        </section>
+        <aside id="seller-form-background" class="lg:mr-10 lg:my-10 lg:w-50/50 lg:h-5xl">
+            <figure id="seller-form-figure" class="lg:h-full">
+                <img src="/storage/img/forms/elegant-tea-party-assortment_original.jpg" alt="Japanese Jewellery Stock Image" class="rounded-r-xl lg:h-full object-cover">
+            </figure>
+        </aside>
+    </main>
 </template>
